a64d4ebd70741d1379a4a173f38727b6bcf02f88
qianyuIm <1040583846@qq.com>
2020-08-20 11:52:27 +0800

    upgrade repo

---
+++ QYNovelModule/Classes/C/QYNovelController.swift
@@ -0,0 +1,19 @@
+//
+//  aaViewController.swift
+//  QYUtilCore
+//
+//  Created by cyd on 2020/8/19.
+//
+
+import UIKit
+import QYUtilCore
+
+open class QYViewController: QYViewController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
